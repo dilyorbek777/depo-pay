94a95cdbc047e17bba19646d51474703cb912724
devdilyorbek <dilyorbekdev@gmail.com>
2024-03-10 23:01:07 +0500

    full landing page + animations + blogs & api + fix errors & bugs + api to hosting domain | mobile view

--- components/site/articles.tsx
+++ components/site/articles.tsx
@@ -20,8 +20,8 @@ export default async function Articles() {
     console.log(drat);
 
     return (
-        <div className='w-full px-16 py-16 flex  items-center justify-center gap-10'>
-            <div className="flex w-1/2">
+        <div className='w-full px-16 py-16 flex max-xl:flex-col  items-center justify-center gap-10'>
+            <div className="flex w-1/2 max-xl:w-2/3 max-md:w-full">
                 {data.map((l) => (
                     <Link href={'/blog/' + l.id} key={l.id}>
                         {l.id === data.length && <div className="w-full flex flex-col items-center gap-5">
@@ -33,12 +33,12 @@ export default async function Articles() {
                     </Link>
                 ))}
             </div>
-            <div className="flex w-1/2 flex-col items-center justify-center gap-5">
+            <div className="flex w-1/2 max-xl:w-2/3 max-md:w-full flex-col items-center justify-center gap-10">
 
                 {drat.map((item) => (
-                    <Link href={'/blog/' + item.id} key={item.id} className="w-full flex  justify-between items-center gap-5">
+                    <Link href={'/blog/' + item.id} key={item.id} className="w-full flex  max-sm:flex-col justify-between items-center gap-5">
                         <CustomImage nameclass="ounded-3xl w-52 h-52 object-cover" img={item.img} title={item.title} />
-                        <div className="flex flex-col items-center justify-between w-full">
+                        <div className="flex flex-col items-center justify-between w-full gap-5">
 
                             <p className='text-[#7D5FFF] bg-[#E5DFF4] px-3 py-1 rounded-full'>{item.category}</p>
                             <p className='text-2xl text-center font-bold'>{item.title.slice(0, 100)}..</p>
